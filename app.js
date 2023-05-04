84c5c0db40f397b59fee379ccad0ee0928c22edc
Kharisma <kiran.nankani28@gmail.com>
2023-05-04 20:18:59 +0200

    added partials folder

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+hbs.registerPartials('views/partials');
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
