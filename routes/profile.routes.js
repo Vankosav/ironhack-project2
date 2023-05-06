6ebde592759282c0c107314bd48f0cf6688b7055
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-05-06 13:39:36 +0200

    create recipe branch

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -5,6 +5,23 @@ const User = require("../models/User.model");
 const Recipe = require("../models/Recipe.model");
 
 
+router.get("/your-recipes", (req, res) => {
+    res.render("profile/new-recipe.hbs");
+});
+
+
+router.post("/your-recipes", async (req, res) => {
+   
+    try {
+         const newRecipe = await Recipe.create(req.body);
+         console.log("Recipe created:", newRecipe);
+         res.redirect("/recipes");
+       }
+       catch (err) {
+         console.log("Recipe couldn't be created:", err);
+         res.send("Error");
+   }
+ });
 
 
 
